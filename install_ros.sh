386be05bf0e0b1732d6d6b59bff89d62d7729d2e
kushal <gkushalg01@gmail.com>
2023-07-30 22:07:41 +0530

    Added notion link

--- install_ros.sh
+++ install_ros.sh
@@ -1,3 +1,6 @@
+# Uploaded to Notion.so
+# https://proud-aerosteon-b68.notion.site/Install-ROS-05d655d529ff452085daf8db4ab1413c?pvs=4
+
 sudo apt update && sudo apt upgrade -y
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt install curl -y
