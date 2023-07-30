5af17c127b7a5ac286dc5d2c5bd702ba20d8f062
kushal <gkushalg01@gmail.com>
2023-07-30 21:39:38 +0530

    commented skype

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -1,9 +1,9 @@
 sudo apt update && sudo apt upgrade -y
 sudo apt install cmake openssh-server net-tools curl snapd ubuntu-restricted-extras gnome-tweak-tool synaptic vlc shutter open-vm-tools-desktop -y
 #skype
-wget https://repo.skype.com/latest/skypeforlinux-64.deb
-sudo dpkg -i skypeforlinux-64.deb
-sudo apt-get install -f
+# wget https://repo.skype.com/latest/skypeforlinux-64.deb
+# sudo dpkg -i skypeforlinux-64.deb
+# sudo apt-get install -f
 #chrome 
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
