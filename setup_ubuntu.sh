b2f975cf21bc3b7d6f570dcad746f9d27b5fb887
Kushal Ghosh <57442239+gkushalg01@users.noreply.github.com>
2023-08-16 22:40:29 +0530

    Update setup_ubuntu.sh

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -2,7 +2,9 @@
 # https://proud-aerosteon-b68.notion.site/Setup-Ubuntu-88d230f1c59d4ef1a4fbc94b47efd6e2?pvs=4
 
 sudo apt update && sudo apt upgrade -y
+sudo add-apt-repository universe
 sudo apt install cmake openssh-server net-tools curl snapd ubuntu-restricted-extras gnome-tweak-tool synaptic vlc shutter open-vm-tools-desktop -y
+# gnome tweak tool has been renamed under universe repo as gnome-tweaks
 #skype
 # wget https://repo.skype.com/latest/skypeforlinux-64.deb
 # sudo dpkg -i skypeforlinux-64.deb
